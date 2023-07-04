4af4b6b33aa18866380e5df085e4fece00aa0a85
Rahul Bhonsale <satriales@Rahuls-Air.local>
2023-07-04 23:02:37 +0200

    add rpc service impl to binary

--- src/example/KillService.scala
+++ src/example/KillService.scala
@@ -1,5 +1,14 @@
 package example
 
-class KillService {
+import protos.touse.person.{Person, PersonStatus}
+import protos.touse.person.PersonServiceGrpc.PersonService
 
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+class KillService extends PersonService {
+  override def killT(request: Person): Future[PersonStatus] = Future {
+    val isDead = request.age > 77 || request.name.equalsIgnoreCase("bill")
+    PersonStatus(isDead)
+  }
 }
